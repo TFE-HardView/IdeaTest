6e09337578488b675ae27364b8fc15227efa6d2f
TFE-HardView <1610200651@qq.com>
2019-06-21 13:48:02 +0800

    Initial commit

---
+++ src/main/java/TestCase.java
@@ -0,0 +1,15 @@
+import com.alibaba.fastjson.JSON;
+import org.dom4j.Document;
+import org.junit.Test;
+
+public class TestCase {
+    public static void main(String[] args){
+        System.out.println("你好啊！");
+        Document document;
+        JSON.toJSONString(new TestCase());
+    }
+    @Test
+    public void a(){
+
+    }
+}
